27fc60f5364b8d8de853265f3a1492064473d6ec
Dang Huynh <hhdang6637@gmail.com>
2019-11-12 09:54:10 +0700

    change screen resolution

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Xvfb :1 -screen 0 800x600x16 > /tmp/Xvfb.log 2>&1 &
-x11vnc -shared -forever -display :1 &
+Xvfb :1 -screen 0 800x600x16 > /tmp/Xvfb_$$.log 2>&1 &
+x11vnc -shared -forever -display :1 > /tmp/x11vnc_$$.log 2>&1 &
