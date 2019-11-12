d1c2dd2b4a72c6efe9d9ed0830953e38444da1e2
Dang Huynh <hhdang6637@gmail.com>
2019-11-12 09:54:26 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --name=selenium -p 5900:5900 -v $PWD/selenium_fb.py:/root/selenium_fb.py test_selenium_fb
+docker run --rm -it --name=selenium -p 5900:5900 -v $PWD:/root/$(basename $PWD) test_selenium_fb
